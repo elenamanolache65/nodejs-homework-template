e9c3c5d4c48c69c564421d3d3e51b42721ea07f5
elenamanolache65 <elenamanolache53@gmail.com>
2025-05-11 19:30:29 +0300

    up

--- models/contacts.js
+++ models/contacts.js
@@ -12,7 +12,7 @@ const listContacts = async () => {
 
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
-  return contacts.find(contact => contact.id === contactId) || null;
+   return contacts.find(contact => contact.id === contactId) || null;
 };
 
 const removeContact = async (contactId) => {
